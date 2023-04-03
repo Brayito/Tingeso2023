33c68ed2f714e8c414b9fcafa794a7d7fdfb6d1b
Bryan <bryan.sepulveda@usach.cl>
2023-04-03 01:20:05 -0400

    Inicialización proyecto

---
+++ demo (2)/demo/src/main/java/controllers/ProveedorController.java
@@ -0,0 +1,5 @@
+package controllers;
+
+import entities.ProveedorEntity;
+import repositories.ProveedorRepository;
+import services.ProveedorService;
---
+++ demo (2)/demo/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO proveedor(codigoProveedores,nombre,clasificacion,retencion) VALUES ("01001", "Juan Alberto", "A","Si");
+INSERT INTO proveedor(codigoProveedores,nombre,clasificacion,retencion) VALUES ("01002", "Roberto Paredes", "C","No");
+INSERT INTO proveedor(codigoProveedores,nombre,clasificacion,retencion) VALUES ("01003", "Miguel Antonio", "D","Si");
+INSERT INTO proveedor(codigoProveedores,nombre,clasificacion,retencion) VALUES ("02001", "Javier Militao", "B","No");
+INSERT INTO proveedor(codigoProveedores,nombre,clasificacion,retencion) VALUES ("02002", "Edgar Ponce", "B","Si");
